342c3b40887df4391fd3f7ae9bdb78764f8b6495
chill <supchillstudio@gmail.com>
2022-11-20 12:35:12 +0500

    jjj

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-DOMRectReadOnly;
+const numberOfFilms = +prompt("How many films did you watch?", "");
+const personMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+for (let i = 1; i < 2; i++) {
+  const a = prompt("Which is your fav?", ""),
+    b = +prompt("How much will you give", "");
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+console.log(personMovieDB);
+let final;
+if (numberOfFilms < 10) {
+  final = alert("Вы лох");
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+  final = alert("Вы мисье");
+} else {
+  final = alert("errssor");
+}
