af1d96f3edb1b37202bac62f9998bf8674d72632
chill <supchillstudio@gmail.com>
2022-11-22 09:31:05 +0500

    Last

--- js/script.js
+++ js/script.js
@@ -6,17 +6,45 @@ const personMovieDB = {
   genres: [],
   privat: false,
 };
-for (let i = 1; i < 2; i++) {
-  const a = prompt("Which is your fav?", ""),
-    b = +prompt("How much will you give", "");
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("error");
-    i--;
-  }
-}
+// for (let i = 1; i < 2; i++) {
+//   const a = prompt("Which is your fav?", ""),
+//     b = +prompt("How much will you give", "");
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     i--;
+//   }
+// }
+// part#2
+// let num = 1;
+// while (num <= 2) {
+//   const a = prompt("Which is your fav?", ""),
+//     b = +prompt("How much will you give", "");
+//   num++;
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     num--;
+//   }
+// }
+//part#3
+// let num = 1;
+// do {
+//   const a = prompt("Which is your fav?", ""),
+//     b = +prompt("How much will you give", "");
+//   num++;
+//   if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//     personMovieDB.movies[a] = b;
+//     console.log("done");
+//   } else {
+//     console.log("error");
+//     num--;
+//   }
+// } while (num <= 2);
 console.log(personMovieDB);
 let final;
 if (numberOfFilms < 10) {
